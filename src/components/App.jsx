a5e8637ce8b59658363bb9802a772acb55cf2fdc
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-15 19:43:23 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -70,12 +70,13 @@ class App extends Component {
   toggleModal = evt => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
     if (!this.state.showModal) {
-      // const itemId = evt.currentTarget.id;
-      // this.state.foto.map(item => {
-      //   if (item.id === Number(itemId)) {
-      //     this.setState({ fotoModal: item.largeImageURL });
-      //   }
-      // });
+      const itemId = evt.currentTarget.id;
+      // eslint-disable-next-line array-callback-return
+      this.state.foto.map(item => {
+        if (item.id === Number(itemId)) {
+          this.setState({ fotoModal: item.largeImageURL });
+        }
+      });
     }
   };
   render() {
